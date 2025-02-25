6a8fa5caaa12578c013123b2e392c793fad9bcc2
Mm <maxkulezenin@gmail.com>
2025-02-25 11:58:46 +0700

    Create MenuUIController

---
+++ Assets/Game/Scripts/UI/Controllers/MenuUIController.cs
@@ -0,0 +1,33 @@
+﻿using Game.Scripts.UI.LevelMenuWindow;
+using Game.Scripts.UI.MVVM;
+using Game.Scripts.UI.Root;
+using Game.Scripts.UI.StartMenuWindow;
+
+namespace Game.Scripts.UI.Controllers
+{
+    public class MenuUIController
+    {
+        private readonly UIRootViewModel _rootViewModel;
+
+        public MenuUIController(UIRootViewModel rootViewModel)
+        {
+            _rootViewModel = rootViewModel;
+        }
+
+        public StartMenuViewModel OpenStartMenu()
+        {
+            StartMenuViewModel viewModel = new StartMenuViewModel(this);
+            _rootViewModel.OpenWindow(viewModel);
+
+            return viewModel;
+        }
+
+        public LevelMenuViewModel OpenLevelMenu()
+        {
+            LevelMenuViewModel viewModel = new LevelMenuViewModel(this);
+            _rootViewModel.OpenWindow(viewModel);
+
+            return viewModel;
+        }
+    }
+}
