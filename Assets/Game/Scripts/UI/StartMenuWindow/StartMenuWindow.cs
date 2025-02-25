853c7853a2c65812854afeafffbe21905fa6b7a7
Mm <maxkulezenin@gmail.com>
2025-02-25 11:58:29 +0700

    StartMenuWindow

---
+++ Assets/Game/Scripts/UI/StartMenuWindow/StartMenuWindow.cs
@@ -0,0 +1,27 @@
+﻿using Game.Scripts.UI.MVVM;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Scripts.UI.StartMenuWindow
+{
+    public class StartMenuWindow: View<StartMenuViewModel>
+    {
+        [SerializeField] private Button _openLevelMenuButton;
+
+        private void Start()
+        {
+            _openLevelMenuButton.onClick.AddListener(OpenLevelMenuButtonClicked);
+        }
+
+        private void OpenLevelMenuButtonClicked()
+        {
+            ViewModel.OpenLevelMenuRequest();
+        }
+
+        protected override void OnUnBindViewModel()
+        {
+            _openLevelMenuButton.onClick.RemoveListener(OpenLevelMenuButtonClicked);
+            Destroy(gameObject);
+        }
+    }
+}
