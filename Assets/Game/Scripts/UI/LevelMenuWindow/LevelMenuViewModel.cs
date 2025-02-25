5de23d40ce45f4b13852dea1236caea27d1dddaf
Mm <maxkulezenin@gmail.com>
2025-02-25 11:58:20 +0700

    LevelMenuWindow

---
+++ Assets/Game/Scripts/UI/LevelMenuWindow/LevelMenuViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Game.Scripts.UI.Controllers;
+using Game.Scripts.UI.MVVM;
+
+namespace Game.Scripts.UI.LevelMenuWindow
+{
+    public class LevelMenuViewModel : ViewModel
+    {
+        public override string PrefabName => "LevelMenu";
+
+        private readonly MenuUIController _uiController;
+
+        public LevelMenuViewModel(MenuUIController uiController)
+        {
+            _uiController = uiController;
+        }
+
+        public void ExitToStartMenuRequest()
+        {
+            _uiController.OpenStartMenu();
+        }
+    }
+}
